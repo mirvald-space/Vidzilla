5813bde2239b5071093e4c8bc8a9eedf6657b386
mirvald-space <mirvald.vadim@icloud.com>
2024-08-06 15:30:20 +0300

    fix instagram

--- handlers/instagram.py
+++ handlers/instagram.py
@@ -25,7 +25,6 @@ async def process_instagram(message, bot, instagram_url):
                             video_url = item['video_versions'][0]['url']
                             caption = item['caption']['text'] if 'caption' in item and item['caption'] else ""
 
-                            # Отправка видео
                             video_file = URLInputFile(video_url)
                             await bot.send_video(
                                 chat_id=message.chat.id,
@@ -33,7 +32,6 @@ async def process_instagram(message, bot, instagram_url):
                                 caption=caption
                             )
 
-                            # Отправка видео как документа
                             file_name = f"instagram_video_{
                                 message.from_user.id}.mp4"
                             doc_file = URLInputFile(
@@ -41,15 +39,15 @@ async def process_instagram(message, bot, instagram_url):
                             await bot.send_document(
                                 chat_id=message.chat.id,
                                 document=doc_file,
-                                caption="Вот ваше Instagram видео в виде файла",
+                                # caption="Вот ваше Instagram видео в виде файла",
                                 disable_content_type_detection=True
                             )
                         else:
-                            await bot.send_message(message.chat.id, "Это публикация не содержит видео.")
+                            await bot.send_message(message.chat.id, "This publication does not contain a video.")
                     else:
-                        await bot.send_message(message.chat.id, "Не удалось найти медиа в ответе API.")
+                        await bot.send_message(message.chat.id, "Failed to find media in the API response.")
                 else:
                     error_message = await response.text()
                     await bot.send_message(message.chat.id, f"API Error: HTTP {response.status}\nDetails: {error_message}")
     except Exception as e:
-        await bot.send_message(message.chat.id, f"Ошибка при обработке Instagram видео: {str(e)}")
+        await bot.send_message(message.chat.id, f"Error while processing Instagram videos: {str(e)}")
