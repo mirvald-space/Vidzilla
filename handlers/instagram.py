d43ebdda98f4b34317fe606094776b58f0778927
mirvald-space <mirvald.vadim@icloud.com>
2024-08-09 09:47:01 +0200

    fix inst

--- handlers/instagram.py
+++ handlers/instagram.py
@@ -14,13 +14,12 @@ async def process_instagram(message, bot, instagram_url):
     try:
         url = "https://instagram-video-or-images-downloader.p.rapidapi.com/"
 
-        payload = f"-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"url\"\r\n\r\n{
-            instagram_url}\r\n-----011000010111000001101001--\r\n\r\n"
+        payload = {"url": instagram_url}
 
         headers = {
             "x-rapidapi-key": RAPIDAPI_KEY,
             "x-rapidapi-host": "instagram-video-or-images-downloader.p.rapidapi.com",
-            "Content-Type": "multipart/form-data; boundary=---011000010111000001101001"
+            "content-type": "application/x-www-form-urlencoded"
         }
 
         async with aiohttp.ClientSession() as session:
@@ -29,39 +28,44 @@ async def process_instagram(message, bot, instagram_url):
                     data = await response.json()
                     logger.info(f"API response: {json.dumps(data, indent=2)}")
 
-                    if 'video' in data:
-                        video_url = data['video']
-                        logger.info(
-                            f"Attempting to access video URL: {video_url}")
+                    if data['status'] and data['code'] == 200:
+                        response_data = data['response']
+                        if 'links' in response_data and len(response_data['links']) > 0:
+                            video_url = response_data['links'][0]['url']
+                            video_title = response_data['title']
+                            logger.info(
+                                f"Attempting to access video URL: {video_url}")
 
-                        try:
-                            video_file = URLInputFile(video_url)
-                            await bot.send_video(
-                                chat_id=message.chat.id,
-                                video=video_file,
-                                caption="Here's your Instagram video"
-                            )
-                            logger.info("Video sent successfully")
+                            try:
+                                video_file = URLInputFile(video_url)
+                                await bot.send_video(
+                                    chat_id=message.chat.id,
+                                    video=video_file,
+                                    caption=f"Here's your Instagram video: {
+                                        video_title}"
+                                )
+                                logger.info("Video sent successfully")
 
-                            file_name = f"instagram_video_{
-                                message.from_user.id}.mp4"
-                            doc_file = URLInputFile(
-                                video_url, filename=file_name)
-                            await bot.send_document(
-                                chat_id=message.chat.id,
-                                document=doc_file,
-                                caption="Here's your Instagram video as a file",
-                                disable_content_type_detection=True
-                            )
-                            logger.info("Document sent successfully")
-                        except Exception as send_error:
-                            logger.error(
-                                f"Error sending video/document: {str(send_error)}")
-                            await bot.send_message(message.chat.id, f"Error sending video/document: {str(send_error)}")
-                    elif 'image' in data:
-                        await bot.send_message(message.chat.id, "This publication contains an image, not a video.")
+                                file_name = f"instagram_video_{
+                                    message.from_user.id}.mp4"
+                                doc_file = URLInputFile(
+                                    video_url, filename=file_name)
+                                await bot.send_document(
+                                    chat_id=message.chat.id,
+                                    document=doc_file,
+                                    caption=f"Here's your Instagram video as a file: {
+                                        video_title}",
+                                    disable_content_type_detection=True
+                                )
+                                logger.info("Document sent successfully")
+                            except Exception as send_error:
+                                logger.error(
+                                    f"Error sending video/document: {str(send_error)}")
+                                await bot.send_message(message.chat.id, f"Error sending video/document: {str(send_error)}")
+                        else:
+                            await bot.send_message(message.chat.id, "This publication does not contain a video.")
                     else:
-                        await bot.send_message(message.chat.id, "This publication does not contain a video or image.")
+                        await bot.send_message(message.chat.id, f"API Error: {data.get('msg', 'Unknown error')}")
                 else:
                     error_message = await response.text()
                     logger.error(f"API Error: HTTP {
